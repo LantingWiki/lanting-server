be4e074d3d5d67b36ba2d6a7e86fe0425e880f3d
boyang <wangboyang1991@gmail.com>
2020-10-08 23:58:01 +0800

    FEATURE: add mapperscan

--- src/main/java/wiki/lanting/LantingServerApplication.java
+++ src/main/java/wiki/lanting/LantingServerApplication.java
@@ -1,13 +1,17 @@
 package wiki.lanting;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author wang.boyang
+ */
 @SpringBootApplication
+@MapperScan(basePackages = {"wiki.lanting.mappers"})
 public class LantingServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LantingServerApplication.class, args);
 	}
-
 }
