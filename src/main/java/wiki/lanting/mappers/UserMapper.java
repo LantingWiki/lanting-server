d899f246f2223addfbf602bd17a5aa8bb03096ac
boyang <wangboyang1991@gmail.com>
2020-10-08 23:56:00 +0800

    FEATURE: user mapper

---
+++ src/main/java/wiki/lanting/mappers/UserMapper.java
@@ -0,0 +1,7 @@
+package wiki.lanting.mappers;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import wiki.lanting.models.UserEntity;
+
+public interface UserMapper extends BaseMapper<UserEntity> {
+}
