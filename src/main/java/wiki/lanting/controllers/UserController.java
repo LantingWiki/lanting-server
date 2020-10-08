d2eed0441b648bbabf4f53e666d8d947972ef923
boyang <wangboyang1991@gmail.com>
2020-10-08 23:57:25 +0800

    FEATURE: user controller

--- src/main/java/wiki/lanting/controllers/UserController.java
+++ src/main/java/wiki/lanting/controllers/UserController.java
@@ -1,17 +1,19 @@
 package wiki.lanting.controllers;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import wiki.lanting.common.LantingResponse;
-import wiki.lanting.model.UserEntity;
+import wiki.lanting.models.UserEntity;
 import wiki.lanting.services.UserService;
 
 /**
  * @author wang.boyang
  */
+@Slf4j
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
@@ -28,8 +30,19 @@ public class UserController {
         return new LantingResponse<UserEntity>().data(user);
     }
 
+    @PostMapping("/create")
+    public LantingResponse<UserEntity> createUser(@RequestBody CreateUserRequestBody requestBody) {
+        UserEntity user = userService.createUser(requestBody);
+        return new LantingResponse<UserEntity>().data(user);
+    }
+
     @Data
     private static class ReadUserRequestBody {
         public long id;
+    }
+
+    @Data
+    public static class CreateUserRequestBody {
+        public String nickname;
     }
 }
