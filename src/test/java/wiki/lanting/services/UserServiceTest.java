afc765048b2162f01b32eeae26bc77d8d87b8dbb
Marcus Aurelianus <37787424+marcus-aurelianus@users.noreply.github.com>
2020-10-09 22:25:50 +0800

    FEATURE WIP function getrecordLength and generate dummy names

--- src/test/java/wiki/lanting/services/UserServiceTest.java
+++ src/test/java/wiki/lanting/services/UserServiceTest.java
@@ -13,6 +13,11 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import java.util.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.io.*;
+
 @Slf4j
 @SpringBootTest
 class UserServiceTest {
@@ -28,6 +33,25 @@ class UserServiceTest {
     @AfterAll
     static void tearDown() {
 
+    }
+
+
+    public static List<String> readFileInList(String fileName)
+    {
+
+        List<String> lines = Collections.emptyList();
+        try
+        {
+            lines = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+        }
+
+        catch (IOException e)
+        {
+
+            // do something
+            e.printStackTrace();
+        }
+        return lines;
     }
 
     @Test
@@ -51,6 +75,31 @@ class UserServiceTest {
 
         readUser = userService.readUser(actual.id);
         assertNull(readUser);
+
+        List<String> first_name = readFileInList("src\\test\\java\\wiki\\lanting\\constants\\first_name.txt");
+
+        List<String> last_name = readFileInList("src\\test\\java\\wiki\\lanting\\constants\\last_name.txt");
+
+        log.info(" first-name file {}.",first_name.get(0) );
+
+        log.info(" last-name file {} with length {}.", last_name.get(0).split(" ")[0], last_name.size());
+        int userLength = userService.getUserRecordLength();
+        log.info("Current user length: {}", userLength);
+        long startTime = System.nanoTime();
+        if (userLength < 1000000){
+            int counter = 0;
+            for (int j = 0; j < 100000000; j++) {
+                counter += 1;
+            }
+            log.info("{} operations done.",counter );
+        }
+
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime);
+        log.info("Time spend on alter action {} ms", duration/1000000 );
+        int userLengthAfter = userService.getUserRecordLength();
+        log.info("Current user length after alter: {}", userLengthAfter);
+
     }
 
     @Test
