cec55d98ad9ed8454ba9101faeb423df145b3a5c
boyang <wangboyang1991@gmail.com>
2020-10-09 15:33:03 +0800

    FIX: add @component to remove warning

--- src/main/java/wiki/lanting/mappers/UserMapper.java
+++ src/main/java/wiki/lanting/mappers/UserMapper.java
@@ -1,7 +1,12 @@
 package wiki.lanting.mappers;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Component;
 import wiki.lanting.models.UserEntity;
 
+/**
+ * @author wang.boyang
+ */
+@Component("userMapper")
 public interface UserMapper extends BaseMapper<UserEntity> {
 }
