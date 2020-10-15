2b49f55c429b187df4a091e25936dd0a06e6dc55
boyang <wangboyang1991@gmail.com>
2020-10-15 22:46:09 +0800

    FEATURE WIP: consumes XML

--- src/main/java/wiki/lanting/controllers/WechatController.java
+++ src/main/java/wiki/lanting/controllers/WechatController.java
@@ -1,21 +1,14 @@
 package wiki.lanting.controllers;
 
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import wiki.lanting.common.LantingResponse;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
-import java.io.StringReader;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
@@ -63,19 +56,14 @@ public class WechatController {
         }
     }
 
-    @PostMapping(path = {"/message/receive", "/echo"}, consumes = {MediaType.APPLICATION_XML_VALUE})
-    public LantingResponse<String> readPlainMsg(@RequestBody WechatController.ReadPlainMsgRequestBody requestBody) throws ParserConfigurationException, IOException, SAXException {
-        log.info("received xml: {}", requestBody.xml);
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        Document document = builder.parse(new InputSource(new StringReader(requestBody.xml)));
-        Element rootElement = document.getDocumentElement();
-
+    @PostMapping(path = {"/message/receive", "/echo"}, consumes = {MediaType.TEXT_XML_VALUE})
+    public LantingResponse<String> readPlainMsg(@RequestBody String requestBody) throws ParserConfigurationException, IOException, SAXException {
+        log.info("received xml: {}", requestBody);
+//        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+//        DocumentBuilder builder = factory.newDocumentBuilder();
+//        Document document = builder.parse(new InputSource(new StringReader(requestBody.xml)));
+//        Element rootElement = document.getDocumentElement();
         return new LantingResponse<String>().data("");
     }
 
-    @Data
-    private static class ReadPlainMsgRequestBody {
-        public String xml;
-    }
 }
